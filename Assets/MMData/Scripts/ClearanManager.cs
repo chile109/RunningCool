d837bf17557ff47194a228ba6e8608ce498c3267
Kevin Chang <kevin@megalive.com.tw>
2018-05-23 23:54:52 +0800

    [Add] level1 新增過關特效\邏輯

---
+++ Assets/MMData/Scripts/ClearanManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MoreMountains.InfiniteRunnerEngine;
+using MoreMountains.Tools;
+
+public class ClearanManager : MonoBehaviour {
+
+	public float Goal = 1500;
+	private bool complete = false;
+	void Update () {
+		if (GameManager.Instance.Points >= Goal && !complete)
+			WinEffect(LevelManager.Instance.CurrentPlayableCharacters[0]);
+	}
+
+
+	/// <summary>
+	/// Kills the player.
+	/// </summary>
+	public virtual void WinEffect(PlayableCharacter player)
+	{
+		complete = true;
+		LevelManager.Instance.CurrentPlayableCharacters.Remove(player);
+		Destroy(player.gameObject);
+		MMEventManager.TriggerEvent(new MMGameEvent("Win"));
+		StartCoroutine(RemoveCharacterCo(player));
+	}
+
+	/// <summary>
+	/// Coroutine that kills the player, stops the camera, resets the points.
+	/// </summary>
+	/// <returns>The player co.</returns>
+	protected virtual IEnumerator RemoveCharacterCo(PlayableCharacter player)
+	{
+		yield return new WaitForSeconds(1f);
+
+	}
+}
