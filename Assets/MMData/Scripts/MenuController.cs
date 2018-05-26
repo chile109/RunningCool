7ec8c7353d9fb23573bfb426288dd8ea50addc99
Kevin Chang <kevin@megalive.com.tw>
2018-05-26 14:22:25 +0800

    [Add] 菜單頁面控制器、完成菜單頁面 [Remove] 不必要場景

---
+++ Assets/MMData/Scripts/MenuController.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine.SceneManagement;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuController : MonoBehaviour {
+	
+	public Button btn_start;
+	public Button btn_exit;
+
+	public Button select_level1;
+	public Button select_level2;
+	public Button select_level3;
+
+	public GameObject InitView;
+	public GameObject SelectView;
+
+
+	public CamZoom Zoomtool;
+
+	void Start () {
+		btn_start.onClick.AddListener(delegate
+		{
+
+			InitView.SetActive(false);
+			SelectView.SetActive(true);
+			Zoomtool.Zoom(0.5f);
+
+		});
+
+		btn_exit.onClick.AddListener(delegate
+		{
+
+			Application.Quit();
+
+		});
+
+		select_level1.onClick.AddListener(delegate {
+
+			SceneManager.LoadSceneAsync("Level1");
+		});
+
+		select_level2.onClick.AddListener(delegate {
+		
+			SceneManager.LoadSceneAsync("Level2");
+		});
+
+		select_level3.onClick.AddListener(delegate {
+		
+			SceneManager.LoadSceneAsync("Level3");
+		});
+	}
+}
