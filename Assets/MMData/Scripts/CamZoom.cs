89c51907dd43d22bce776396aeb45c71dce00863
Kevin Chang <kevin@megalive.com.tw>
2018-05-26 14:20:38 +0800

    [Add] 背景移動＆聚焦腳本

---
+++ Assets/MMData/Scripts/CamZoom.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamZoom : MonoBehaviour
+{
+	
+	public Vector3 TargetScale;
+	private Transform ZoomBG;
+	private Vector3 InitScale;
+
+
+	private void Start()
+	{
+		ZoomBG = this.transform;
+		InitScale = ZoomBG.localScale;
+	}
+
+
+	public void Zoom(float _time)
+	{
+		Debug.Log(_time);
+		StartCoroutine(ZIn(_time));
+	}
+
+	IEnumerator ZIn(float _duration)
+	{
+		float elapsedTime = 0;
+
+		while (elapsedTime < _duration)
+		{
+			Debug.Log(elapsedTime);
+			ZoomBG.localScale = Vector3.Lerp(InitScale, TargetScale, elapsedTime / _duration);
+			elapsedTime += Time.deltaTime;
+
+			yield return new WaitForEndOfFrame();
+		} 
+
+		Debug.Log("End");
+	}
+}
---
+++ Assets/MMData/Scripts/ForMenuParralle.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine.UI;
+using UnityEngine;
+
+public class ForMenuParralle : MonoBehaviour
+{
+
+	/// the relative speed of the object
+	public float Speed = 0;
+	public static ForMenuParralle CurrentParallaxOffset;
+
+	protected RawImage _rawImage;
+	protected Renderer _renderer;
+	protected Vector2 _newOffset;
+
+	protected float _position = 0;
+	protected float yOffset;
+
+	/// <summary>
+	/// On start, we store the current offset
+	/// </summary>
+	protected virtual void Start()
+	{
+		CurrentParallaxOffset = this;
+		if (GetComponent<Renderer>() != null)
+		{
+			_renderer = GetComponent<Renderer>();
+		}
+
+		if (_renderer == null && GetComponent<RawImage>() != null)
+		{
+			_rawImage = GetComponent<RawImage>();
+		}
+
+	}
+
+	/// <summary>
+	/// On update, we apply the offset to the texture
+	/// </summary>
+	protected virtual void Update()
+	{
+		if ((_rawImage == null) && (_renderer == null))
+		{
+			return;
+		}
+
+		_position += (Speed / 300) * Time.deltaTime;
+
+
+
+		// position reset
+		if (_position > 1.0f)
+		{
+			_position -= 1.0f;
+		}
+
+		// we apply the offset to the object's texture
+		_newOffset.x = _position;
+		_newOffset.y = yOffset;
+
+		if (_renderer != null)
+		{
+			_renderer.material.mainTextureOffset = _newOffset;
+		}
+		if (_rawImage != null)
+		{
+			_rawImage.material.mainTextureOffset = _newOffset;
+		}
+
+	}
+}
