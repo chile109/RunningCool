873a0e71e820267eb52d25840aead5af33572b00
Kevin Chang <kevin@megalive.com.tw>
2018-06-03 18:07:47 +0800

    [Add] 不掉命外掛

---
+++ Assets/MMData/Scripts/InvincibleTool.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MoreMountains.InfiniteRunnerEngine
+{
+	public class InvincibleTool : MonoBehaviour
+	{
+		public bool AvoidTwice = false;
+		// Update is called once per frame
+		void Update()
+		{
+			if (Input.GetKeyDown(KeyCode.Tab) && !AvoidTwice)
+			{
+				GameManager.Instance.Invincible = true;
+				AvoidTwice = true;
+			}
+
+			else if(Input.GetKeyDown(KeyCode.Tab) && AvoidTwice)
+			{
+				GameManager.Instance.Invincible = false;
+				AvoidTwice = false;
+			}
+			
+		}
+	}
+}
